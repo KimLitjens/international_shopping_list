0f5dc70ac20bd55e6b34a10ba3f3d90bbec506a0
Kim Litjens <kimlitjens4@gmail.com>
2021-11-08 19:22:48 +0100

    add fb context and signup page

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function signUp() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+
     return (
-        <div>
-            <h1>Sign Up Page</h1>
-        </div>
-    )
+        <form className="mt-4" onSubmit={handleSubmit(onSubmit)}>
+            <input className="mr-4" {...register("firstName")} placeholder="first name" />
+            <input className="mr-4" {...register("lastName")} placeholder="last name" />
+            <input className="mr-4" {...register("userName")} placeholder="user name" />
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <input className="bg-yellow-500" type="submit" />
+        </form>
+    );
 }
