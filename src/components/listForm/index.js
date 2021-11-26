b63359ae210ccf15ddbadb4e47b90b42e569351b
Kim Litjens <kimlitjens4@gmail.com>
2021-11-26 18:18:04 +0100

    made form input responsive

--- src/components/listForm/index.js
+++ src/components/listForm/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import styles from './listForm.styles'
 
 export default function ListForm({ handleSubmit, onSubmit, register, errors }) {
     return (
-        <form className="mt-4" onSubmit={handleSubmit(onSubmit)}>
-            <input className="mr-4" {...register("French")} placeholder="French" />
-            <input className="mr-4" {...register("German")} placeholder="German" />
-            <input className="mr-4" {...register("Dutch")} placeholder="Dutch" />
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <input className={styles.input} {...register("French")} placeholder="French" />
+            <input className={styles.input} {...register("German")} placeholder="German" />
+            <input className={styles.input} {...register("Dutch")} placeholder="Dutch" />
             {errors.exampleRequired && <span>This field is required</span>}
 
-            <input className="bg-yellow-500 px-2 mx-2" type="submit" />
+            <input className={styles.submit} type="submit" />
         </form>
     )
 }
---
+++ src/components/listForm/listForm.styles.js
@@ -0,0 +1,13 @@
+const styles = {
+    form: [
+        `flex flex-col md:flex-row mt-4`,
+    ],
+    input: [
+        `my-2 md: mr-4`
+    ],
+    submit: [
+        `bg-yellow-500 px-2 my-2 md:mx-2`
+    ]
+}
+
+export default styles
