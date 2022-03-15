5c4dc864ed9e865e0ca23c40b46df252ad7bd6c1
Kim Litjens <kimlitjens4@gmail.com>
2022-03-15 17:17:13 +0100

    changed style switch

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import DarkModeToggle from 'react-dark-mode-toggle'
+
 
 import styles from './header.styles'
 import { SignOutButton } from '..'
+import { Switch } from '../'
 import { useAuth } from '../../utils/hooks/useAuth'
 
 export default function Header() {
     const userInfo = useAuth();
     const [auth, setAuth] = useState({});
-    const [isDarkMode, setIsDarkMode] = useState(() => false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
 
     useEffect(() => {
@@ -24,12 +25,11 @@ export default function Header() {
         : 'Welcome';
     return (
         <div className={styles.div}>
-            <DarkModeToggle
-                onChange={setIsDarkMode}
-                checked={isDarkMode}
-                size={40}
-                className={`my-auto ml-2`}
+            <Switch
+                isDarkMode={isDarkMode}
+                setIsDarkMode={setIsDarkMode}
             />
+
             <h2 className={styles.welcome}>{welcomeMessage}</h2>
             <h1 className={styles.h1}>My International Shopping List</h1>
             <SignOutButton />
